d11ede093b3e09b85116e7aea4c3c38844a605f4
sojiwoo <58384366+jiwooproity@users.noreply.github.com>
2022-07-06 15:02:16 +0900

    Converter-0027/Styled-Components Css 분리 작업

--- src/App.js
+++ src/App.js
@@ -1,195 +1,30 @@
 import React, { useRef, useState } from "react";
+
 import _ from "lodash";
-import styled from "styled-components";
-import SelectBox from "./SelectBox";
-import ReactJson from "react-json-view";
 import * as XLSX from "xlsx";
+
 import { kr, en, de, es, fr, ja, pt, zh } from "./defaultLocale";
 
-const ConverterContainer = styled.div`
-  width: 100%;
-  height: 100vh;
-  padding: 20px 20px 20px 20px;
+import {
+  ConvertMainWrapper,
+  ConverterBottomWrap,
+  ConverterContainer,
+  ConverterTitleWrap,
+  ConverterTitle,
+  ConverterWrapper,
+  LanguageMenu,
+  LanguageRequired,
+  LanguageTarget,
+  UploadFileInput,
+  UploadFileInputStyle,
+  UploadFileInputText,
+  UploadFileInputButton,
+  JsonView,
+  DownloadButton,
+} from "./style/Styled";
 
-  position: relative;
-
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-
-  background-color: rgba(0, 0, 0, 0.1);
-`;
-
-const ConvertMainWrapper = styled.div`
-  border-radius: 10px;
-  box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
-    rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
-
-  overflow: hidden;
-`;
-
-const ConverterWrapper = styled.div`
-  padding: 20px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-
-  background-color: white;
-`;
-
-const ConverterTitleWrap = styled.div`
-  padding: 20px;
-
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  background-color: rgba(0, 0, 0, 0.8);
-`;
-
-const ConverterBottomWrap = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const ConverterTitle = styled.h1`
-  font-size: 15px;
-  color: white;
-`;
-
-const LanguageMenu = styled.div`
-  width: 100%;
-  display: flex;
-  justify-content: flex-end;
-  align-items: center;
-  list-style: none;
-
-  padding: 4px 0px;
-`;
-
-const LanguageRequired = styled.span`
-  font-size: 14px;
-  margin-right: 2px;
-
-  color: rgb(165, 0, 52);
-`;
-
-const LanguageTarget = styled.span`
-  font-size: 11px;
-  line-height: 12px;
-
-  display: block;
-  font-weight: 700;
-`;
-
-const DownloadButton = styled.a`
-  width: 100%;
-  height: ${({ disabled }) => (disabled ? "0px" : "40px")};
-
-  line-height: 40px;
-
-  text-align: center;
-  font-size: 12px;
-
-  text-decoration: none;
-
-  color: white;
-
-  cursor: pointer;
-
-  background-color: rgba(0, 0, 0, 0.8);
-
-  pointer-events: ${({ disabled }) => (disabled ? "none" : "")};
-
-  &:hover {
-    background-color: #a50034;
-    color: white;
-  }
-
-  transition: background-color 0.5s ease, color 0.5s ease, height 0.5s ease;
-`;
-
-const JsonView = styled.div`
-  width: 487.66px;
-  margin-top: 15px;
-  max-height: 300px;
-  overflow-y: scroll;
-
-  padding: 10px;
-
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  border-radius: 5px;
-
-  &::-webkit-scrollbar {
-    width: 5px;
-    height: 5px;
-    background-color: transparent;
-  }
-
-  &::-webkit-scrollbar-thumb {
-    border-radius: 3px;
-    background-color: rgba(0, 0, 0, 0.1);
-
-    &:hover {
-      background-color: rgba(0, 0, 0, 0.5);
-    }
-  }
-`;
-
-const UploadFileInput = styled.input`
-  width: 380px;
-  padding: 8px 5px 8px 5px;
-  margin-left: 10px;
-  display: none;
-`;
-
-const UploadFileInputStyle = styled.label`
-  width: 380px;
-  font-size: 12px;
-  margin-left: 10px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const UploadFileInputText = styled.span`
-  width: 320px;
-  font-size: 12px;
-  padding: 8px 5px 8px 5px;
-
-  border-bottom: ${({ required }) =>
-    required
-      ? "1px solid rgba(165, 0, 52, 0.3)"
-      : "1px solid rgba(0, 0, 0, 0.1)"};
-
-  &:hover {
-    border-color: rgba(0, 0, 0, 0.5);
-  }
-
-  &:focus {
-    border-color: rgba(0, 0, 0, 0.5);
-  }
-
-  transition: border-color 0.5s ease;
-`;
-
-const UploadFileInputButton = styled.button`
-  padding: 7px 14px;
-  border: none;
-  border-radius: 4px;
-  cursor: pointer;
-  background-color: rgba(0, 0, 0, 0.8);
-  color: white;
-
-  &:hover {
-    background-color: #a50034;
-    color: white;
-  }
-
-  transition: background-color 0.5s ease;
-`;
+import SelectBox from "./SelectBox";
+import ReactJson from "react-json-view";
 
 const App = () => {
   const [excelData, setExcelData] = useState([]);
---
+++ src/style/Styled.js
@@ -0,0 +1,186 @@
+import styled from "styled-components";
+
+export const ConverterContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 20px 20px 20px 20px;
+
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background-color: rgba(0, 0, 0, 0.1);
+`;
+
+export const ConvertMainWrapper = styled.div`
+  border-radius: 10px;
+  box-shadow: rgba(0, 0, 0, 0.4) 0px 2px 4px,
+    rgba(0, 0, 0, 0.3) 0px 7px 13px -3px, rgba(0, 0, 0, 0.2) 0px -3px 0px inset;
+
+  overflow: hidden;
+`;
+
+export const ConverterWrapper = styled.div`
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+
+  background-color: white;
+`;
+
+export const ConverterTitleWrap = styled.div`
+  padding: 20px;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  background-color: rgba(0, 0, 0, 0.8);
+`;
+
+export const ConverterBottomWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ConverterTitle = styled.h1`
+  font-size: 15px;
+  color: white;
+`;
+
+export const LanguageMenu = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  list-style: none;
+
+  padding: 4px 0px;
+`;
+
+export const LanguageRequired = styled.span`
+  font-size: 14px;
+  margin-right: 2px;
+
+  color: rgb(165, 0, 52);
+`;
+
+export const LanguageTarget = styled.span`
+  font-size: 11px;
+  line-height: 12px;
+
+  display: block;
+  font-weight: 700;
+`;
+
+export const DownloadButton = styled.a`
+  width: 100%;
+  height: ${({ disabled }) => (disabled ? "0px" : "40px")};
+
+  line-height: 40px;
+
+  text-align: center;
+  font-size: 12px;
+
+  text-decoration: none;
+
+  color: white;
+
+  cursor: pointer;
+
+  background-color: rgba(0, 0, 0, 0.8);
+
+  pointer-events: ${({ disabled }) => (disabled ? "none" : "")};
+
+  &:hover {
+    background-color: #a50034;
+    color: white;
+  }
+
+  transition: background-color 0.5s ease, color 0.5s ease, height 0.5s ease;
+`;
+
+export const JsonView = styled.div`
+  width: 487.66px;
+  margin-top: 15px;
+  max-height: 300px;
+  overflow-y: scroll;
+
+  padding: 10px;
+
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+
+  &::-webkit-scrollbar {
+    width: 5px;
+    height: 5px;
+    background-color: transparent;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 3px;
+    background-color: rgba(0, 0, 0, 0.1);
+
+    &:hover {
+      background-color: rgba(0, 0, 0, 0.5);
+    }
+  }
+`;
+
+export const UploadFileInput = styled.input`
+  width: 380px;
+  padding: 8px 5px 8px 5px;
+  margin-left: 10px;
+  display: none;
+`;
+
+export const UploadFileInputStyle = styled.label`
+  width: 380px;
+  font-size: 12px;
+  margin-left: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const UploadFileInputText = styled.span`
+  width: 320px;
+  font-size: 12px;
+  padding: 8px 5px 8px 5px;
+
+  border-bottom: ${({ required }) =>
+    required
+      ? "1px solid rgba(165, 0, 52, 0.3)"
+      : "1px solid rgba(0, 0, 0, 0.1)"};
+
+  &:hover {
+    border-color: rgba(0, 0, 0, 0.5);
+  }
+
+  &:focus {
+    border-color: rgba(0, 0, 0, 0.5);
+  }
+
+  transition: border-color 0.5s ease;
+`;
+
+export const UploadFileInputButton = styled.button`
+  padding: 7px 14px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  background-color: rgba(0, 0, 0, 0.8);
+  color: white;
+
+  &:hover {
+    background-color: #a50034;
+    color: white;
+  }
+
+  transition: background-color 0.5s ease;
+`;
